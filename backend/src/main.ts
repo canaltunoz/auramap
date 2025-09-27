1ad868c8bd31c980a44741484cce40f3f2eb0862
Can ALTUNÖZ <m.canaltunoz@gmail.com>
2025-09-27 17:31:16 +0300

    fix(backend): explicitly use ExpressAdapter to select HTTP driver and resolve Nest v11 PackageLoader error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
 
   app.useGlobalPipes(
     new ValidationPipe({
